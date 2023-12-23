3531d7ae29e99771e87c3bfe2cdb3beefacacd36
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-12-23 18:10:35 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,50 @@
 import {
-    Canister,
-    update,
-    query,
-    Record,
-    text,
-    nat32,
-    int32,
-    StableBTreeMap,
-    Err,
-    Result,
-    Variant,
-    Principal,
-    Ok,
-    Vec,
-    Opt
+  Canister,
+  update,
+  query,
+  Record,
+  text,
+  nat32,
+  int32,
+  StableBTreeMap,
+  Err,
+  Result,
+  Variant,
+  Principal,
+  Ok,
+  Vec,
+  Opt,
 } from 'azle';
 
 const Movie = Record({
-    movieId: Principal,
-    name: text,
-    price: nat32,
-    durationMinutes: int32,
-    firstShowTime: text,
-    showAmount: int32,
-    seatAmount: int32,
-    scheduleIds: Vec(Principal)
-})
+  movieId: Principal,
+  name: text,
+  price: nat32,
+  durationMinutes: int32,
+  firstShowTime: text,
+  showAmount: int32,
+  seatAmount: int32,
+  scheduleIds: Vec(Principal),
+});
 type Movie = typeof Movie.tsType;
 
 const Schedule = Record({
-    scheduleId: Principal,
-    movieId: Principal,
-    startTime: text,
-    endTime: text,
-    availableSeat: int32
-})
+  scheduleId: Principal,
+  movieId: Principal,
+  startTime: text,
+  endTime: text,
+  availableSeat: int32,
+});
 type Schedule = typeof Schedule.tsType;
 
 const CinemaTicketError = Variant({
-    MovieDoesNotExist: Principal,
-    NoAvailableSeat: text,
-    NotPositiveInt: text,
-    TimeFormatError: text,
-    DurationError: text,
-    ShowAmountError: text,
-    ScheduleDoesNotExist: Principal
+  MovieDoesNotExist: Principal,
+  NoAvailableSeat: text,
+  NotPositiveInt: text,
+  TimeFormatError: text,
+  DurationError: text,
+  ShowAmountError: text,
+  ScheduleDoesNotExist: Principal,
 });
 type CinemaTicketError = typeof CinemaTicketError.tsType;
 
@@ -52,190 +52,208 @@ let movieList = StableBTreeMap<Principal, Movie>(0);
 let scheduleList = StableBTreeMap<Principal, Schedule>(1);
 
 export default Canister({
+  addMovie: update(
+    [text, nat32, int32, text, int32, int32],
+    Result(Movie, CinemaTicketError),
+    (name, price, durationMinutes, firstShowTime, showAmount, seatAmount) => {
+      // CHECK ERROR
+      if (durationMinutes <= 0 || showAmount <= 0 || seatAmount <= 0) {
+        return Err({
+          NotPositiveInt:
+            'durationMinutes, showAmount, seatAmount must be positive integer.',
+        });
+      }
+      if (!isValidTimeFormat(firstShowTime)) {
+        return Err({
+          TimeFormatError:
+            "Please use 'HH:mm' format to input firstShowTime.",
+        });
+      }
 
-    addMovie: update([text, nat32, int32, text, int32, int32], Result(Movie, CinemaTicketError), (name, price, durationMinutes, firstShowTime, showAmount, seatAmount) => {
-        
-        // CHECK ERROR
-        if (durationMinutes <= 0 || showAmount <= 0 || seatAmount <= 0) {
-            return Err({
-                NotPositiveInt: "durationMinutes, showAmount, seatAmount must be positive integer."
-            })
-        }
-        if (isValidTimeFormat(firstShowTime) == false) {
-            return Err({
-                TimeFormatError: "Please use 'HH:mm' format to input firstShowTime."
-            })
-        }
+      if (durationMinutes > 720) {
+        return Err({
+          DurationError:
+            'The movie duration should be less than 12 hours(720 minutes).',
+        });
+      }
 
-        if (durationMinutes > 720) {
-            return Err({
-                DurationError: "The movie duration should be less than 12 hours(720 minutes)."
-            })
-        }
+      const maxAmount =
+        (timeToMinutes('24:00') - timeToMinutes(firstShowTime)) /
+        (durationMinutes + 30); // add 30 minutes as break for cleaning the studio
+      if (showAmount > maxAmount) {
+        return Err({
+          ShowAmountError: `The inputted amount exceeds the maximum show amount allowed for one day(${maxAmount}).`,
+        });
+      }
 
-        const maxAmount = (timeToMinutes('24:00') - timeToMinutes(firstShowTime))/(durationMinutes+30); //add 30 minutes as break for cleaning the studio
-        if (showAmount > maxAmount) {
-            return Err({
-                ShowAmountError: `The inputted amount exceeds the maximum show amount allowed for one day(${maxAmount}).`
-            })
-        }
+      // INSERT TO MOVIELIST
+      const movieId = generateId();
+      const movie: Movie = {
+        movieId,
+        name,
+        price,
+        durationMinutes,
+        firstShowTime,
+        showAmount,
+        seatAmount,
+        scheduleIds: [],
+      };
+      movieList.insert(movie.movieId, movie);
 
-        // INSERT TO MOVIELIST
-        const movieId = generateId();
-        const movie: Movie = {
-            movieId,
-            name,
-            price,
-            durationMinutes,
-            firstShowTime,
-            showAmount,
-            seatAmount,
-            scheduleIds: [],
-        };
-        movieList.insert(movie.movieId, movie);
-        
-        // GENERATE SCHEDULE
-        for (let i = 0; i<showAmount; i++) {
-            const movieOpt = movieList.get(movieId);
-
-            if ('None' in movieOpt) {
-                return Err({
-                    MovieDoesNotExist: movieId
-                });
-            }
-
-            const movie = movieOpt.Some;
-
-            const scheduleId = generateId();
-            const startTime = minutesToTime(timeToMinutes(movie.firstShowTime) + i*(durationMinutes+30));
-            const endTime = minutesToTime(timeToMinutes(startTime) + durationMinutes);
-            const availableSeat = movie.seatAmount;
-
-            const schedule: Schedule = {
-                scheduleId,
-                movieId,
-                startTime,
-                endTime,
-                availableSeat
-            };
-            scheduleList.insert(schedule.scheduleId, schedule);
-
-            const updatedMovie: Movie = {
-                ...movie,
-                scheduleIds: [...movie.scheduleIds, schedule.scheduleId]
-            };
-
-            movieList.insert(updatedMovie.movieId, updatedMovie);
-        }
-        
-        const movieOpt = movieList.get(movieId);
-        if ('None' in movieOpt) {
-            return Err({
-                MovieDoesNotExist: movieId
-            });
-        }
-        const generatedMovie = movieOpt.Some;
-        
-        return Ok(generatedMovie);
-    }),
-
-    deleteMovie: update([Principal], Result(Movie, CinemaTicketError), (movieId) => {
+      // GENERATE SCHEDULE
+      for (let i = 0; i < showAmount; i++) {
         const movieOpt = movieList.get(movieId);
 
         if ('None' in movieOpt) {
-            return Err({
-                MovieDoesNotExist: movieId
-            });
+          return Err({
+            MovieDoesNotExist: movieId,
+          });
         }
 
         const movie = movieOpt.Some;
 
-        movie.scheduleIds.forEach((scheduleId) => {
-            scheduleList.remove(scheduleId);
-        })
+        const scheduleId = generateId();
+        const startTime = minutesToTime(
+          timeToMinutes(movie.firstShowTime) + i * (durationMinutes + 30)
+        );
+        const endTime = minutesToTime(
+          timeToMinutes(startTime) + durationMinutes
+        );
+        const availableSeat = movie.seatAmount;
 
-        movieList.remove(movie.movieId);
+        const schedule: Schedule = {
+          scheduleId,
+          movieId,
+          startTime,
+          endTime,
+          availableSeat,
+        };
+        scheduleList.insert(schedule.scheduleId, schedule);
 
-        return Ok(movie);
-    }),
+        const updatedMovie: Movie = {
+          ...movie,
+          scheduleIds: [...movie.scheduleIds, schedule.scheduleId],
+        };
 
-    getMovieList: query([], Vec(Movie), () => {
-        return movieList.values();
-    }),
+        movieList.insert(updatedMovie.movieId, updatedMovie);
+      }
 
-    getScheduleList: query([], Vec(Schedule), () => {
-        return scheduleList.values();
-    }),
+      const movieOpt = movieList.get(movieId);
+      if ('None' in movieOpt) {
+        return Err({
+          MovieDoesNotExist: movieId,
+        });
+      }
+      const generatedMovie = movieOpt.Some;
 
-    getMovieDetails: query([Principal], Opt(Movie), (movieId) => {
-        return movieList.get(movieId);
-    }),
+      return Ok(generatedMovie);
+    }
+  ),
 
-    getScheduleDetails: query([Principal], Opt(Schedule), (scheduleId) => {
-        return scheduleList.get(scheduleId);
-    }),
-    
-    bookTicket: update([Principal, int32], Result(Schedule, CinemaTicketError), (scheduleId, seats) => {
+  deleteMovie: update(
+    [Principal],
+    Result(Movie, CinemaTicketError),
+    (movieId) => {
+      const movieOpt = movieList.get(movieId);
 
-        // CHECK ERROR
-        const scheduleOpt = scheduleList.get(scheduleId);
-        if ('None' in scheduleOpt) {
-            return Err({
-                ScheduleDoesNotExist: scheduleId
-            });
-        }
+      if ('None' in movieOpt) {
+        return Err({
+          MovieDoesNotExist: movieId,
+        });
+      }
 
-        if (seats <= 0) {
-            return Err({
-                NotPositiveInt: "seats must be positive integer."
-            })
-        }
+      const movie = movieOpt.Some;
 
-        const schedule = scheduleOpt.Some;
-        if (schedule.availableSeat < seats) {
-            return Err({
-                NoAvailableSeat: "No enough available seats."
-            });
-        }
+      movie.scheduleIds.forEach((scheduleId) => {
+        scheduleList.remove(scheduleId);
+      });
 
-        // UPDATE AVAILABLESEATS
-        let updatedSchedule = schedule;
-        updatedSchedule.availableSeat -= seats;
-        scheduleList.insert(updatedSchedule.scheduleId, updatedSchedule);
+      movieList.remove(movie.movieId);
 
-        return Ok(updatedSchedule);
-    })
+      return Ok(movie);
+    }
+  ),
+
+  getMovieList: query([], Vec(Movie), () => {
+    return movieList.values();
+  }),
+
+  getScheduleList: query([], Vec(Schedule), () => {
+    return scheduleList.values();
+  }),
+
+  getMovieDetails: query([Principal], Opt(Movie), (movieId) => {
+    return movieList.get(movieId);
+  }),
+
+  getScheduleDetails: query([Principal], Opt(Schedule), (scheduleId) => {
+    return scheduleList.get(scheduleId);
+  }),
+
+  bookTicket: update(
+    [Principal, int32],
+    Result(Schedule, CinemaTicketError),
+    (scheduleId, seats) => {
+      // CHECK ERROR
+      const scheduleOpt = scheduleList.get(scheduleId);
+      if ('None' in scheduleOpt) {
+        return Err({
+          ScheduleDoesNotExist: scheduleId,
+        });
+      }
+
+      if (seats <= 0) {
+        return Err({
+          NotPositiveInt: 'seats must be a positive integer.',
+        });
+      }
+
+      const schedule = scheduleOpt.Some;
+      if (schedule.availableSeat < seats) {
+        return Err({
+          NoAvailableSeat: 'No enough available seats.',
+        });
+      }
+
+      // UPDATE AVAILABLESEATS
+      let updatedSchedule = schedule;
+      updatedSchedule.availableSeat -= seats;
+      scheduleList.insert(updatedSchedule.scheduleId, updatedSchedule);
+
+      return Ok(updatedSchedule);
+    }
+  ),
 });
 
 function generateId(): Principal {
-    const randomBytes = new Array(29)
-        .fill(0)
-        .map((_) => Math.floor(Math.random() * 256));
+  const randomBytes = new Array(29)
+    .fill(0)
+    .map((_) => Math.floor(Math.random() * 256));
 
-    return Principal.fromUint8Array(Uint8Array.from(randomBytes));
+  return Principal.fromUint8Array(Uint8Array.from(randomBytes));
 }
 
 function isValidTimeFormat(time: text): boolean {
-    const timeRegex = /^([01]\d|2[0-3]):[0-5]\d$/;
+  const timeRegex = /^([01]\d|2[0-3]):[0-5]\d$/;
 
-    return timeRegex.test(time);
+  return timeRegex.test(time);
 }
 
 function timeToMinutes(time: text): int32 {
-    const [hours, minutes] = time.split(':').map(Number);
-    return hours*60 + minutes;
+  const [hours, minutes] = time.split(':').map(Number);
+  return hours * 60 + minutes;
 }
 
 function minutesToTime(minutes: any): text {
-    let hours : any = Math.floor(minutes/60);
-    minutes -= 60*hours;
+  let hours: any = Math.floor(minutes / 60);
+  minutes -= 60 * hours;
 
-    if (hours < 10) {
-        hours = '0' + hours;
-    }
-    if (minutes < 10) {
-        minutes = '0' + minutes;
-    }
+  if (hours < 10) {
+    hours = '0' + hours;
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes;
+  }
 
-    return `${hours}:${minutes}`;
+  return `${hours}:${minutes}`;
 }
